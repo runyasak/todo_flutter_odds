e337f39025e7696ede5facfd18d3c448190e4a6c
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-04-24 09:44:23 +0700

    feat: add todo card part

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,14 +9,30 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(
-            'assets/images/cover.png',
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.cover,
-          ),
-        ],
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/images/cover.png',
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 1.7,
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(32),
+                ),
+                child: Text('Todo'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
