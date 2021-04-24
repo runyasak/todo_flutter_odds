45210d9ffeabfab6c7085e766cb2b8b641e2e1eb
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-04-24 10:33:33 +0700

    feat: implement TextField

--- lib/screens/new_todo.dart
+++ lib/screens/new_todo.dart
@@ -9,48 +9,62 @@ class _NewTodoScreenState extends State<NewTodoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        child: Stack(
-          children: [
-            Image.asset(
-              'assets/images/cover.png',
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.cover,
-            ),
-            Positioned(
-              bottom: 0,
-              child: Container(
-                height: MediaQuery.of(context).size.height / 1.7,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/images/cover.png',
                 width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.all(24),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(32),
-                ),
-                child: Column(
-                  children: [],
-                ),
+                fit: BoxFit.cover,
               ),
-            ),
-            Positioned(
-              top: MediaQuery.of(context).padding.top,
-              child: Container(
-                padding: EdgeInsets.only(
-                  left: 16,
-                  top: 8,
-                ),
-                child: IconButton(
-                  icon: Icon(
-                    Icons.chevron_left,
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 1.7,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.all(24),
+                  decoration: BoxDecoration(
                     color: Colors.white,
-                    size: 32,
+                    borderRadius: BorderRadius.circular(32),
                   ),
-                  onPressed: () => Navigator.of(context).pop(),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Topic',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              Positioned(
+                top: MediaQuery.of(context).padding.top,
+                child: Container(
+                  padding: EdgeInsets.only(
+                    left: 16,
+                    top: 8,
+                  ),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.chevron_left,
+                      color: Colors.white,
+                      size: 32,
+                    ),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
